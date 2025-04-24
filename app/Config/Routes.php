13ee933be405a19c3bc86304740f64381ec5ef11
jordipa94 <jpujol6@inscaparrella.cat>
2025-04-24 17:24:58 +0200

    routes users

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -41,3 +41,27 @@ $routes->get('/papeleraNoticies', 'NoticiesController::recycleBinNoticia');
 $routes->get('/restaurarNoticia/(:num)', 'NoticiesController::restaurarNoticia/$1');
 $routes->get('/readNoticia/(:num)', 'NoticiesController::readNoticia/$1');
 $routes->get('/searchNoticiaCrud', 'NoticiesController::searchNoticiaCrud');
+
+//LOGIN ADMIN
+$routes->get('/administracio_log','AdministracioController::login_dashboard');
+$routes->post('/administracio_log/(:num)','AdministracioController::login_dashboard/$1');
+$routes->get('/administracio_log_dark','AdministracioController::login_dashboard_dark');
+$routes->post('/administracio_log_post','AdministracioController::login_post');
+//REGISTRACION ADMIN
+$routes->get('/registrar','AdministracioController::Registrar');
+$routes->post('/registrar_post','AdministracioController::Registrar_post');
+//ADMINISTRACION
+$routes->get('/administracio','AdministracioController::index');
+//ADMINISTRACION USERS
+$routes->get('/administracio_users','AdministracioController::users_Admin');
+//CRUS USERS
+$routes->get('/Ver_users/(:num)', 'AdministracioController::readUser/$1');
+//BUSCADOR USER 
+$routes->get('/searchUser','AdministracioController::searchUser');
+//CRUD USERS
+$routes->get('/deleteUser/(:num)', 'AdministracioController::deleteUser/$1');
+//CALENDARIO 
+
+$routes->get('/calendar', 'AdministracioController::calendar');
+$routes->get('/calendar/loadEvents', 'AdministracioController::loadEvents');
+$routes->post('/calendar/addEvent', 'AdministracioController::addEvent');
